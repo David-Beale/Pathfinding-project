75233387909529f99b81da0805da680507dfe9f9
David B <david_beale@outlook.com>
2020-02-19 14:33:49 +0000

    add persistence

--- Scripts/main.js
+++ Scripts/main.js
@@ -19,29 +19,26 @@ const drawTrafficLightBottom = (x, y, color) => {
   c.stroke();
   c.fill()
 }
-
-const drawMap = (counter) => {
-  c.fillStyle = 'rgb(0,0,0)'
-  c.fillRect(200, 100, 500, 100)
-  c.fillRect(100, 600, 500, 100)
-  c.fillRect(100, 100, 100, 500)
-  c.fillRect(600, 200, 100, 500)
-  c.fillRect(600, 200, 600, 100)
-  c.fillRect(1200, 200, 100, 400)
-  c.fillRect(900, 200, 100, 400)
-  c.fillRect(900, 500, 500, 100)
-  c.fillRect(1000, 500, 100, 400)
-  c.fillRect(1100, 900, -600, 100)
-  c.fillRect(500, 900, 100, -300)
-  drawTrafficLightBottom(700, 200, 'red')
-  if (counter >= 0 && counter <= 40) drawTrafficLightBottom(700, 200, 'red')
-  if (counter > 40 && counter <= 60) drawTrafficLightBottom(700, 200, 'yellow')
-  if (counter > 60 && counter <= 160) drawTrafficLightBottom(700, 200, 'green')
-  if (counter > 160 && counter <= 180) drawTrafficLightBottom(700, 200, 'yellow')
-  if (counter > 180 && counter <= 240) {
-    drawTrafficLightBottom(700, 200, 'red')
-    counter = 0;
-  }
+let counter = 0;
+const drawMap = () => {
+  c.fillStyle = 'rgb(0,0,0)';
+  c.fillRect(200, 100, 500, 100);
+  c.fillRect(100, 600, 500, 100);
+  c.fillRect(100, 100, 100, 500);
+  c.fillRect(600, 200, 100, 500);
+  c.fillRect(600, 200, 600, 100);
+  c.fillRect(1200, 200, 100, 400);
+  c.fillRect(900, 200, 100, 400);
+  c.fillRect(900, 500, 500, 100);
+  c.fillRect(1000, 500, 100, 400);
+  c.fillRect(1100, 900, -600, 100);
+  c.fillRect(500, 900, 100, -300);
+  drawTrafficLightBottom(700, 200, 'red');
+  if (counter >= 0 && counter <= 100) drawTrafficLightBottom(700, 200, 'red');
+  if (counter > 100 && counter <= 120) drawTrafficLightBottom(700, 200, 'yellow');
+  if (counter > 120 && counter <= 220) drawTrafficLightBottom(700, 200, 'green');
+  if (counter > 220 && counter <= 240) drawTrafficLightBottom(700, 200, 'yellow');
+  if (counter === 240) counter = 0;
 }
 
 const drawCircle = (x, y, radius) => {
@@ -52,13 +49,12 @@ const drawCircle = (x, y, radius) => {
   c.stroke();
   c.fill()
 }
-const drawBlackCircle = (x, y, radius) => {
+
+const drawOutlineCircle = (x, y, radius) => {
   c.beginPath();
   c.arc(x, y, radius, 0, Math.PI * 2, false);
-  c.fillStyle = 'rgb(0,0,0)'
-  c.strokeStyle = 'rgb(0,0,0)';
+  c.strokeStyle = 'blue';
   c.stroke();
-  c.fill()
 }
 
 //////////////////////////////////////////////////////////////////*START*  SET UP GRAPH////////////////////////////////////////////////////////////////
@@ -129,27 +125,47 @@ const nextDirection = (currentVertex, nextVertex) => {
   }
   return dx, dy, targetX, targetY;
 }
-let counter = 0;
+
 function animate () {
   const animation = requestAnimationFrame(animate);
-
   c.clearRect(0, 0, innerWidth, innerHeight);
   drawMap(counter);
 
-  if (counter >= 0 && counter <= 40) map.graphObj[25].light = 'red'
-  if (counter > 40 && counter <= 60) map.graphObj[25].light = 'yellow'
-  if (counter > 60 && counter <= 160) map.graphObj[25].light = 'green'
-  if (counter > 160 && counter <= 180) map.graphObj[25].light = 'yellow'
-  if (counter > 180 && counter <= 240) map.graphObj[25].light = 'red'
+  if (counter >= 0 && counter <= 100) map.graphObj[25].light = 'red'
+  if (counter > 100 && counter <= 120) map.graphObj[25].light = 'yellow'
+  if (counter > 120 && counter <= 220) map.graphObj[25].light = 'green'
+  if (counter > 220 && counter <= 240) map.graphObj[25].light = 'yellow'
 
   counter++;
-  drawCircle(x, y, radius);
+  if (initializing) {
+    if (initialRadius > radius) {
+      initialRadius = 0;
+      initializing = false;
+    }
+    drawCircle(clickX, clickY, initialRadius);
+    initialRadius += pulseSpeed;
+  }
 
-  if (reachedDestination === true) {
+  if (initialClick && !initializing) {
+    drawCircle(x, y, radius);
+  }
+  if (pulseCircle) {
+    if (initialRadius >= radius / 1.5) {
+      pulseSpeed = -pulseSpeed;
+    }
+    if (initialRadius < 0) {
+      initialRadius = 0;
+      pulseSpeed = 5;
+      pulseCircle = false;
+    }
+    if (pulseSpeed < 0) drawOutlineCircle(clickX, clickY, (radius / 1.5))
+    drawCircle(clickX, clickY, initialRadius);
+    initialRadius += pulseSpeed;
+  }
+
+  if (reachedDestination) {
     dx = 0;
     dy = 0;
-    counter = 0;
-    cancelAnimationFrame(animation)
   }
   if (requireNewPath) {
     let currentVertexName = pathArray[i]
@@ -165,42 +181,21 @@ function animate () {
       }
     }
   }
+  if (!reachedDestination) {
+    x += dx;
+    y += dy;
+    if (x === targetX && y === targetY) {
+      requireNewPath = true;
+      i++;
+    }
+    if (x === finalX && y === finalY) {
+      requireNewPath = false;
+      reachedDestination = true;
+    }
+  }
 
-  x += dx;
-  y += dy;
-  if (x === targetX && y === targetY) {
-    requireNewPath = true;
-    i++;
-  }
-  if (x === finalX && y === finalY) {
-    requireNewPath = false;
-    reachedDestination = true;
-  }
 }
-function growCircle () {
-  const circleAnimation = requestAnimationFrame(growCircle);
-  if (initialRadius >= radius) {
-    initialRadius = 0;
-    cancelAnimationFrame(circleAnimation)
-  }
-  drawCircle(clickX, clickY, initialRadius);
-  initialRadius += pulseSpeed;
-}
-function pulseCircle () {
-  const pulseAnimation = requestAnimationFrame(pulseCircle);
-  if (initialRadius >= radius / 1.5) {
-    pulseSpeed = -pulseSpeed;
-  }
-  if (initialRadius < 0) {
-    initialRadius = 0;
-    pulseSpeed = 5;
-    cancelAnimationFrame(pulseAnimation)
-    animate();
-  }
-  if (pulseSpeed < 0) drawBlackCircle(clickX, clickY, radius / 1.5)
-  drawCircle(clickX, clickY, initialRadius);
-  initialRadius += pulseSpeed;
-}
+
 //////////////////////////////////////////////////////////////////*END*  ANIMATION////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////*START*  EVENT LISTENER////////////////////////////////////////////////////////////////
@@ -214,8 +209,8 @@ let dx;
 let dy;
 let i = 0;
 
-let requireNewPath = true;
-let reachedDestination = false;
+let requireNewPath = false;
+let reachedDestination = true;
 let direction;
 let targetX;
 let targetY;
@@ -230,18 +225,20 @@ let finalY;
 
 
 const arrayOfVertices = Object.keys(map.graphObj)
+let initializing = false;
 let initialClick = false;
 let secondClick = false;
+let pulseCircle = false;
 let clickX;
 let clickY;
-window.addEventListener('mousedown', function (e) {
+function eventListener (e) {
   if (initialClick === false) {
     clickX = Math.floor((e.pageX / 100)) * 100 + radius;
     clickY = Math.floor((e.pageY / 100)) * 100 + radius;
     for (let i = 0; i < arrayOfVertices.length; i++) {
       if (map.graphObj[arrayOfVertices[i]].x === clickX - radius && map.graphObj[arrayOfVertices[i]].y === clickY - radius) {
         console.log(arrayOfVertices[i])
-        growCircle();
+        initializing = true;
         start = arrayOfVertices[i]
         x = clickX;
         y = clickY;
@@ -258,38 +255,46 @@ window.addEventListener('mousedown', function (e) {
         end = arrayOfVertices[i]
         finalX = clickX;
         finalY = clickY;
+        pathArray = dijkstra(map.graphObj, start, end)[1];
+        requireNewPath = true;
+        reachedDestination = false;
+        secondClick = true;
+        pulseCircle = true;
         i = arrayOfVertices.length
       }
     }
-    secondClick = true;
-    pathArray = dijkstra(map.graphObj, start, end)[1];
-    pulseCircle();
+
 
   }
   else if (initialClick === true && secondClick === true) {
-    start = end;
-    x = clickX;
-    y = clickY;
-    i = 0;
-    requireNewPath = true;
-    reachedDestination = false;
-    clickX = Math.floor((e.pageX / 100)) * 100 + radius;
-    clickY = Math.floor((e.pageY / 100)) * 100 + radius;
-    for (let i = 0; i < arrayOfVertices.length; i++) {
-      if (map.graphObj[arrayOfVertices[i]].x === clickX - radius && map.graphObj[arrayOfVertices[i]].y === clickY - radius) {
-        end = arrayOfVertices[i]
-        finalX = clickX;
-        finalY = clickY;
-        i = arrayOfVertices.length
+    if (reachedDestination) {
+      start = end;
+      x = clickX;
+      y = clickY;
+      i = 0;
+      clickX = Math.floor((e.pageX / 100)) * 100 + radius;
+      clickY = Math.floor((e.pageY / 100)) * 100 + radius;
+      for (let i = 0; i < arrayOfVertices.length; i++) {
+        if (map.graphObj[arrayOfVertices[i]].x === clickX - radius && map.graphObj[arrayOfVertices[i]].y === clickY - radius) {
+          end = arrayOfVertices[i]
+          finalX = clickX;
+          finalY = clickY;
+          requireNewPath = true;
+          reachedDestination = false;
+          pathArray = dijkstra(map.graphObj, start, end)[1];
+          pulseCircle = true;
+          i = arrayOfVertices.length
+        }
       }
     }
-    pathArray = dijkstra(map.graphObj, start, end)[1];
-    pulseCircle();
   }
+}
+window.addEventListener('mousedown', e => {
+  eventListener(e)
 })
 
 //////////////////////////////////////////////////////////////////*END*  EVENT LISTENER////////////////////////////////////////////////////////////////
 
 
 
-// animate();
+animate();
